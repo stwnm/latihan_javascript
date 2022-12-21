6e9d9faddef7895bfa419ba3f8ae44d5e80967a6
stwnm <istiawanmalik029@gmail.com>
2022-12-21 19:18:30 +0800

    Mengedit function

--- function.js
+++ function.js
@@ -1,9 +1,12 @@
 function jumlahVolumeDuaKubus(a,b){
     var volumeA;
     var volumeB;
+    var total;
 
     volumeA = a * a * a;
     volumeB = b * b * b;
+
+    total = volumeA + volumeB;
 }
 
 function jumlahVolumeDuaKubus(8,3);
