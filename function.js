731e9f2403bc78c8e453db8b9af13de217b6adc7
stwnm <istiawanmalik029@gmail.com>
2022-12-21 19:52:28 +0800

    mencoba tipe data array

--- function.js
+++ function.js
@@ -1,14 +1,25 @@
-function jumlahVolumeDuaKubus(a, b) {
-  var volumeA;
-  var volumeB;
-  var total;
+// function jumlahVolumeDuaKubus(a, b) {
+//   var volumeA;
+//   var volumeB;
+//   var total;
 
-  volumeA = a * a * a;
-  volumeB = b * b * b;
+//   volumeA = a * a * a;
+//   volumeB = b * b * b;
 
-  total = volumeA + volumeB;
+//   total = volumeA + volumeB;
 
-  return total;
+//   return total;
+// }
+
+// alert(jumlahVolumeDuaKubus(8, 3));
+
+function tambah() {
+  var hasil = 0;
+  for (i = 0; i < arguments.length; i++) {
+    hasil += Number(prompt("masukkan data"));
+  }
+  return hasil;
 }
 
-alert(jumlahVolumeDuaKubus(8, 3));
+var hasil = tambah(1, 2, 3, 4, 5);
+alert(" hasilnya adalah " + hasil);
